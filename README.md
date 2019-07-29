a87a1bc97fb2db84990101f1cd6ce48e45c68906
thealchemist17 <lucamancinelli17@gmail.com>
2019-07-29 14:34:33 +0200

    bufu

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # rust-todo22
+*Step 1*
+- Use [clap](https://github.com/clap-rs/clap) to parse command line arguments
+- Add an argument `-f` to specify filename using clap library
+- Now try to make tests work again by specifing a named temporary file using [tempfile](https://github.com/Stebalien/tempfile)
+- Add tests for remove, edit and clear
+
+*Step 2*
+- Define a struct that represents a Todo entry
+- Add handling of Priority and State
+- Use serde to serialize and deserialize 
+- Optionally use a config file to specify the path/name of the todo file
+
+*Step 3*
+- Adapt the list subcommand to allow sorting and filtering
+
+*Step 4*
+- Add TUI
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{App, Arg, SubCommand};
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rust-todo22")
+        .version("1.0")
+        .author("Luca Mancinelli <lucamancinelli17@gmail.com>")
+        .about("rust-todo")
+        .arg(Arg::with_name("add").requires("todo"))
+        .arg(Arg::with_name("todo").help("todo"))
+        .arg(Arg::with_name("list"))
+        .arg(Arg::with_name("edit").requires("id"))
+        .arg(Arg::with_name("remove").requires("id"))
+        .arg(Arg::with_name("clear"))
+        .arg(Arg::with_name("id"))
+        .get_matches();
+
+    if matches.is_present("add") {
+        if let Some(matches) = matches.value_of("todo") {
+            println!("{}", matches);
+        }
+    }
 }
